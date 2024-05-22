5250c3c5df78be1a360093690834fe87bf432fc5
Matvii Ivashchenko <matvey.ivashchenko@gmail.com>
2024-05-23 00:04:43 +0200

    enemies recievs damage from bullets

--- ExamGameLevel/ExamGame.cpp
+++ ExamGameLevel/ExamGame.cpp
@@ -3,6 +3,17 @@
 #include <vector>
 #include "enemy.h"
 #include <algorithm>
+#include <random>
+
+
+int getRandom(int a, int b) {           // method used to generate random coordinates of enemies
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_int_distribution<> dist(a, b);
+    int random = dist(gen);
+    return random;
+}
+
 
 void ExamGame(Window &window) {
 
@@ -14,12 +25,15 @@ void ExamGame(Window &window) {
     std::vector<Bullet> bullets;
     std::vector<Enemy> enemies;
     Vector2 direction = { 0, -1 };
+
     int screenWidth = 1600;
     int screenHeight = 900;
     float speed = 2.0f;
+    int bulletDamage = 25;
+    int i = 0;
 
     Rectangle player = { 400, 300, 25, 25 };
-    Enemy enemy = { 1,2,25,30,5,6};
+//    Enemy enemy = { 1,2,25,30,5,6};
 
 
 
@@ -52,7 +66,7 @@ void ExamGame(Window &window) {
             direction = { 1, 0 };
         }
 
-        enemy.moveToPlayer(player.x, player.y);
+//        enemy.moveToPlayer(player.x, player.y);
 
         if (IsKeyPressed(KEY_SPACE)) {
             Bullet newBullet = { { player.x + player.width / 2, player.y, 5, 5 }, {direction.x * 5.0f, direction.y * 5.0f } };
@@ -64,16 +78,37 @@ void ExamGame(Window &window) {
             bullet.rect.y += bullet.speed.y;
         }
 
-        bullets.erase(std::remove_if(bullets.begin(), bullets.end(), [screenWidth, screenHeight](Bullet& b) { return b.rect.y < 0 || b.rect.y > screenHeight || b.rect.x < 0 || b.rect.x > screenWidth; }), bullets.end());
+        bullets.erase(std::remove_if(bullets.begin(), bullets.end(), [screenWidth, screenHeight](Bullet& b) {
+            return b.rect.y < 0 ||
+                    b.rect.y > screenHeight ||
+                    b.rect.x < 0 ||
+                    b.rect.x > screenWidth; }), bullets.end());
+
+        enemies.erase(std::remove_if(enemies.begin(), enemies.end(), [](Enemy& enemy) {
+            return enemy.GetHealth() <= 0;}), enemies.end());
+
+        for(i ; i <10; ++i){
+            enemies.emplace_back(getRandom(-100,0), getRandom(0,screenHeight), 25,30,5,6);
+        }
 
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
         DrawRectangleRec(player, BLUE);
-        DrawRectangleRec(enemy,RED);
+//        DrawRectangleRec(enemy,RED);
 
         for (const auto &bullet : bullets) {
             DrawRectangleRec(bullet.rect, RED);
+        }
+
+        for ( auto &enemy : enemies) {
+            for (auto& bullet : bullets) {
+                if (CheckCollisionRecs(enemy, bullet.rect)) {
+                    enemy.ReceiveDamage(bulletDamage);
+                }
+            }
+            enemy.moveToPlayer(player.x, player.y);
+            DrawRectangleRec(enemy, RED);
         }
 
         EndDrawing();
