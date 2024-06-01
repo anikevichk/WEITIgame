358ccbbd6a6a60b828cbd93863e913e3777c878b
Kate <01194270@pw.edu.pl>
2024-06-01 18:10:21 +0300

    added an explanation screen in maze level

--- levels/MazeLevel/maze.cpp
+++ levels/MazeLevel/maze.cpp
@@ -6,12 +6,21 @@
 #include "renderer.h"
 #include "mazeGenerator.h"
 #include "../../Tools/DrawTools.h"
+#include <cstring>
 
 void Maze(Window &window) {
+
+    Texture2D hotdog, wallH, wallV, sprite, background;
+    Sound mainTheme, bonus, speaking1, speaking2;
+
+    // Load resources
+    ResourceManager::LoadResources(hotdog, wallH, wallV, sprite, background, mainTheme, bonus, speaking1, speaking2);
+
     int screenWidth = 1600;
     int screenHeight = 900;
     float speed = 5.0f;
     SetTargetFPS(60);
+    PlaySound(mainTheme);
 
     int currentFrame = 0;
     float frameRate = 0.1f;
@@ -53,27 +62,48 @@ void Maze(Window &window) {
     // Generate maze
     MazeGenerator::Generate(maze, mazeWidth, mazeHeight);
 
-    Texture2D hotdog, wallH, wallV, sprite, background;
-    Sound mainTheme, bonus;
-
-    // Load resources
-    ResourceManager::LoadResources(hotdog, wallH, wallV, sprite, background, mainTheme, bonus);
 
     std::vector<Rectangle> hotdogs;
     MazeGenerator::GenerateHotdogs(hotdogs, maze, mazeWidth, mazeHeight, 5);
 
     int collectedHotdogs = 0;
     bool gameWon = false;
+    const char* text1 = "Now you need to prepare for the session and colloquia.\n\n"
+                        "Use buttons A and D to move around.\n\n"
+                        "You can also move quickly from one side of the screen to the other.";
 
-//    while(DialogueWindow && !WindowShouldClose()){
-//        BeginDrawing();
-//        DrawTexture(background, 0, 0, WHITE);
-//        const char* text = "Test kskksksksk kkskksk ksksk"
-//                           " \nTestm jdjdjdjdjjdjj jsjsjj"
-//                           "\nskkskkskskskskskkkkkks";
-//        DrawRoundedRectangle(600, 450, 900, 400, 20, text, DialogueWindow, WHITE, "Next");
-//        EndDrawing();
-//    }
+    const char* text2 = "The main thing to remember is to stay focused on your preparation or you\nwon't be able to pass the tests.\n\n"
+                        "Sometimes you'll want to cut down the information through chat gpt.\n\n"
+                        "You should not do that.";
+
+    int textLength1 = strlen(text1);
+    int currentLength1 = 0;
+    int framesCounter1 = 0;
+
+    int textLength2 = strlen(text2);
+    int currentLength2 = 0;
+    int framesCounter2 = 0;
+
+    float letterDelay = 1;
+
+
+    bool DialogueWindow1 = true;
+    bool DialogueWindow2 = false;
+    bool isRightHalf = true;
+
+    SetSoundVolume(mainTheme, 0.3);
+
+    HandleDialogueWindow(text1, currentLength1, textLength1, framesCounter1, speaking2,
+                         background, letterDelay, DialogueWindow1, isRightHalf);
+    if (!DialogueWindow1) {
+        DialogueWindow2 = true;
+        isRightHalf = false;
+    }
+
+    HandleDialogueWindow(text2, currentLength2, textLength2, framesCounter2, speaking1,
+                         background, letterDelay, DialogueWindow2, isRightHalf);
+    StopSound(speaking2);
+    SetSoundVolume(mainTheme, 0.7);
 
     while (!WindowShouldClose()) {
         if (!IsSoundPlaying(mainTheme)) {
@@ -104,5 +134,5 @@ void Maze(Window &window) {
         EndDrawing();
     }
 
-    ResourceManager::UnloadResources(hotdog, wallH, wallV, background, mainTheme, bonus);
+    ResourceManager::UnloadResources(hotdog, wallH, wallV, background, mainTheme, bonus, speaking1, speaking2);
 }
--- levels/MazeLevel/resourceManager.h
+++ levels/MazeLevel/resourceManager.h
@@ -5,8 +5,10 @@
 
 class ResourceManager {
 public:
-    static void LoadResources(Texture2D& hotdog, Texture2D& wallH, Texture2D& wallV, Texture2D& sprite, Texture2D& background, Sound& mainTheme, Sound& bonus);
-    static void UnloadResources(Texture2D& hotdog, Texture2D& wallH, Texture2D& wallV, Texture2D& background, Sound& mainTheme, Sound& bonus);
+    static void LoadResources(Texture2D& hotdog, Texture2D& wallH, Texture2D& wallV, Texture2D& sprite,
+                              Texture2D& background, Sound& mainTheme, Sound& bonus, Sound& speaking1, Sound& speaking2);
+    static void UnloadResources(Texture2D& hotdog, Texture2D& wallH, Texture2D& wallV, Texture2D& background,
+                                Sound& mainTheme, Sound& bonus, Sound& speaking1, Sound& speaking2);
     static void HandlePlayerMovement(Rectangle& player, Vector2& direction, Rectangle*& currentFrames, bool& isKeyPressed, float speed,
                                      Rectangle runForwardFrames[], Rectangle runBackFrames[], Rectangle runLeftFrames[], Rectangle runRightFrames[]);
 };
