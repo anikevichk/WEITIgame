859fe95b1c1c510902e4e379deda3d6be2bfa780
Kate <01194270@pw.edu.pl>
2024-06-04 23:34:04 +0300

    first test works

--- Tests/tests.cpp
+++ Tests/tests.cpp
@@ -1,8 +1,12 @@
-#include "../levels/RunnerLevel/LevelManager.h"
-#include <gtest/gtest.h>
+#include "gtest/gtest.h"
+#include "../levels/CatchLevel/collision.h"
 
+TEST(CollisionTest, MoveSpecialFruitsTest) {
+    Rectangle veryGoodFruit {0, 0, 10, 10};
+    Rectangle veryBadFruit {0, 0, 10, 10};
 
-TEST(LevelManagerTest, IsLossInitiallyFalse) {
-    LevelManager levelManager;
-ASSERT_EQ(levelManager.getCounter(),0);
+    collision::MoveSpecialFruits(veryGoodFruit, veryBadFruit);
+
+    ASSERT_EQ(veryGoodFruit.y, 4);
+    ASSERT_EQ(veryBadFruit.y, 7);
 }
